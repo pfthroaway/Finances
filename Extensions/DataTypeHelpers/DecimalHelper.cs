48e1363b3b3652fc5d9f74bdf3cc0241cd9f1983
PFThroaway <pfthroaway@mail.com>
2017-11-15 14:18:30 -0600

    Update Extensions library

--- Extensions/DataTypeHelpers/DecimalHelper.cs
+++ Extensions/DataTypeHelpers/DecimalHelper.cs
@@ -5,23 +5,17 @@ namespace Extensions.DataTypeHelpers
     /// <summary>Extension class to more easily parse Decimals.</summary>
     public static class DecimalHelper
     {
-        private static NumberStyles _styles = NumberStyles.AllowThousands | NumberStyles.AllowDecimalPoint;
+        /// <summary>Utilizes decimal.TryParse to easily parse a Decimal.</summary>
+        /// <param name="obj">Object to be parsed</param>
+        /// <returns>Parsed Decimal</returns>
+        public static decimal Parse(object obj) => Parse(obj.ToString());
 
         /// <summary>Utilizes decimal.TryParse to easily parse a Decimal.</summary>
         /// <param name="text">Text to be parsed</param>
         /// <returns>Parsed Decimal</returns>
         public static decimal Parse(string text)
         {
-            decimal.TryParse(text, _styles, CultureInfo.InvariantCulture, out decimal temp);
-            return temp;
-        }
-
-        /// <summary>Utilizes decimal.TryParse to easily parse a Decimal.</summary>
-        /// <param name="obj">Object to be parsed</param>
-        /// <returns>Parsed Decimal</returns>
-        public static decimal Parse(object obj)
-        {
-            decimal.TryParse(obj.ToString(), _styles, CultureInfo.InvariantCulture, out decimal temp);
+            decimal.TryParse(text, NumberStyles.AllowDecimalPoint | NumberStyles.AllowThousands, CultureInfo.InvariantCulture, out decimal temp);
             return temp;
         }
     }
